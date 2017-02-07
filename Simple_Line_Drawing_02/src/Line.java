aea0bc054890a3b9954f2fe55824d62f749f0b8b
touhidul-mti <touhidul.mti@gmail.com>
2017-02-08 00:15:43 +0600

    added 2

---
+++ Simple_Line_Drawing_02/src/Line.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.jws.soap.SOAPBinding;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
+import javax.swing.JFrame;
+
+public class Line implements GLEventListener {
+
+	static GLProfile profile = GLProfile.get(GLProfile.GL2);
+	static GLCapabilities capabilities = new GLCapabilities(profile);
+	// The canvas
+	static GLCanvas glcanvas = new GLCanvas(capabilities);
+
+	public static void main(String[] args) {
+		// getting the capabilities object of GL2 profile
+
+		Line l = new Line();
+		// creating frame
+		glcanvas.addGLEventListener(l);
+		glcanvas.setSize(600, 400);
+
+		final JFrame frame = new JFrame("straight Line");
+		// adding canvas to frame
+		frame.getContentPane().add(glcanvas);
+		frame.setSize(frame.getContentPane().getPreferredSize());
+		frame.setVisible(true);
+	}
+
+	// you are to work in this method
+	public void display(GLAutoDrawable drawable) {
+		final GL2 gl = drawable.getGL().getGL2();
+		gl.glBegin(GL2.GL_POINTS);// static field
+
+		// --------your code start from here---------------
+
+		for (int i = 0; i < 100; i++) {
+			Random random = new Random();
+			double x = random.nextDouble() * (1 - (-1)) + (-1);
+			double y = random.nextDouble() * (1 - (-1)) + (-1);
+			System.out.println(x + " " + y);
+			gl.glVertex2d(x, y);
+		}
+
+		File file = new File("coordinates.txt");
+		try {
+			Scanner sc = new Scanner(file);
+
+			while (sc.hasNextLine()) {
+				double x = Double.parseDouble(sc.nextLine());
+				double y = Double.parseDouble(sc.nextLine());
+
+				gl.glVertex2d(x, y);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		gl.glEnd();
+	}
+
+	public void dispose(GLAutoDrawable arg0) {
+		// method body
+	}
+
+	public void init(GLAutoDrawable drawable) {
+		// method body
+		// 4. drive the display() in a loop
+	}
+
+	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
+			int arg4) {
+		// method body
+	}
+	// end of main
+}// end of classimport javax.media.opengl.GL2;
